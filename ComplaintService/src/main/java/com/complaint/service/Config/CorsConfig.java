541afcf5fe6d6b2c13d197553c2dd43ea407460a
ManasiPurkar <141387487+ManasiPurkar@users.noreply.github.com>
2024-05-19 21:39:31 +0530

    Update CorsConfig.java

--- ComplaintService/src/main/java/com/complaint/service/Config/CorsConfig.java
+++ ComplaintService/src/main/java/com/complaint/service/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 // Apply CORS configuration to all paths
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
